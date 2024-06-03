11b92bf93cbc2864786a65a426b82139cf5503bb
Igor Rebas <irebas@student.uw.edu.pl>
2024-06-03 12:27:29 +0000

    new column

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import pandas as pd
 df = pd.DataFrame(data={'cc': ['US', 'FR', 'DE'], 'val': [1, 2, 3]})
 
 df['new_col'] = df['val'].apply(lambda x: x * 100)
+df['continent'] = ['North America' if x == 'US' else 'Europe' for x in df['cc']]
 print(df)
